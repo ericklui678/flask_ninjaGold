64b2b29777021d95e949780007d2b7344b6f517b
Erick Lui <erickjin.lui@gmail.com>
2017-05-12 00:13:39 -0700

    added timestamp to gold history

--- ninjaGold.py
+++ ninjaGold.py
@@ -15,25 +15,26 @@ def index():
 def process():
     global goldNum
     global pTag
+    now = datetime.datetime.now()
     if 'farm' in request.form:
         randomNum = random.randrange(10,21)
         goldNum += randomNum
-        pTag += '<p>Earned ' + str(randomNum) +' gold from farm!</p>'
+        pTag += '<p>Earned ' + str(randomNum) +' gold from farm!  ' +str(now.strftime('%Y/%m/%d %H:%M')) + '</p>'
     if 'cave' in request.form:
         randomNum = random.randrange(5,11)
         goldNum += randomNum
-        pTag += '<p>Earned ' + str(randomNum) +' gold from cave!</p>'
+        pTag += '<p>Earned ' + str(randomNum) +' gold from cave!  ' +str(now.strftime('%Y/%m/%d %H:%M')) + '</p>'
     if 'house' in request.form:
         randomNum = random.randrange(2,6)
         goldNum += randomNum
-        pTag += '<p>Earned ' + str(randomNum) +' gold from house!</p>'
+        pTag += '<p>Earned ' + str(randomNum) +' gold from house!  ' +str(now.strftime('%Y/%m/%d %H:%M')) + '</p>'
     if 'casino' in request.form:
         randomNum = random.randrange(-50,51)
         goldNum += randomNum
         if randomNum > 0:
-            pTag += '<p>Earned ' + str(randomNum) +' gold from casino!</p>'
+            pTag += '<p>Earned ' + str(randomNum) +' gold from casino!  ' +str(now.strftime('%Y/%m/%d %H:%M')) + '</p>'
         else:
-            pTag += '<p>Lost ' + str(abs(randomNum)) +' gold from casino!</p>'
+            pTag += '<p>Lost ' + str(randomNum) +' gold from casino!  ' +str(now.strftime('%Y/%m/%d %H:%M')) + '</p>'
         if goldNum < 0:
             goldNum = 0
     return redirect('/')
