a1ccdc3ebcaf6c7aac83cc6aa7bcc72380e1c4fd
Erick Lui <erickjin.lui@gmail.com>
2017-05-11 23:06:33 -0700

    created css layout

---
+++ ninjaGold.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+
+app.secret_key = 'ThisIsSecret'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print 'Got Post Info'
+    return redirect('/')
+
+app.run(debug = True)
